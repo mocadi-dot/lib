0cc3d01c0eb48cc93da32e84199aa4f154275909
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-24 17:10:10 +0200

    Update 01_pyramids_full.rb

--- 01_pyramids_full.rb
+++ 01_pyramids_full.rb
@@ -2,8 +2,4 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages souhaites-tu ?
 print ">"
 floors = gets.chomp.to_i
 puts "Voici ta pyramide :"
-n = floors
-while n >= 0
-  puts "#" * n
-  n = n - 1
-end
+floors.downto(0) { |n| puts "#" * n } 
